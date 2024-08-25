d6ccfc4d53b5191900a0b2f2d45bc05215d9d29d
Siyabonga <sbngeva@gmail.com>
2024-08-25 15:56:13 +0200

    new commit

--- Tutorials/Tutorial_1/Chapter_14/index.js
+++ Tutorials/Tutorial_1/Chapter_14/index.js
@@ -1,101 +1,128 @@
-// Importing required modules
-const express = require('express'); // Express framework for creating web applications
-const bodyParser = require('body-parser'); // Body parser middleware for parsing incoming request bodies
-const mongoose = require('mongoose'); // Mongoose for MongoDB object modeling
-const fileUpload = require('express-fileupload'); //adds the files property to the req object so that we can access the uploaded files using req.files.
-const homeController = require('./controllers/home');
-const storePostController = require('./controllers/storePost');
-const getPostController = require('./controllers/getPost');
-const newUserController = require('./controllers/newUser'); // import the newUser controller
+const express = require('express');
+const path = require('path');
+const newPostController = require('./controllers/newPost');
+const newUserController = require('./controllers/newUser');
 const storeUserController = require('./controllers/storeUser');
-const loginController = require('./controllers/login');  //import the login controller
-const loginUserController = require('./controllers/loginUser');  // import loginUser
-const expressSession = require('express-session');
+const storePostController = require('./controllers/storePost');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
 const authMiddleware = require('./middleware/authMiddleware');
 const redirectIfAuthenticatedMiddleware = require('./middleware/redirectIfAuthenticatedMiddleware');
-const logoutController = require('./controllers/logout')
+const logoutController = require('./controllers/logout');
 const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost');
+const fileUpload = require('express-fileupload');
 
-// Creating an Express application instance
-const app = new express();
-
-
-//We then register the package in Express with app.use
-app.use(fileUpload())
-
-
-// Setting the view engine to EJS
+const app = express();
+const ejs = require('ejs');
 app.set('view engine', 'ejs');
 
-// Serving static files from the 'public' directory
 app.use(express.static('public'));
-
-// Parsing JSON and URL-encoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Connecting to MongoDB database named 'my_database'
 mongoose.connect('mongodb://localhost/my_database', { useUnifiedTopology: true, useNewUrlParser: true });
 
-// Starting the server and listening on port 4000
-app.listen(4000, () => {
+const PORT = 4000;
+
+// Session middleware
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Flash middleware
+app.use(flash());
+
+global.loggedIn = null;
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId;
+    next();
+});
+
+app.use(fileUpload());
+
+app.listen(PORT, () => {
     console.log('App listening on port 4000');
 });
 
-//the new controller functions
-app.get('/', homeController)
+// Routes
 
-//register the route
-app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController);
-
-// Route for rendering the blog post creation form
-app.get('/create', (req, res) => {
-    res.render('create');
+app.get('/', async (req, res) => {
+    const blogposts = await BlogPost.find({});
+    res.render('index', {
+        blogposts: blogposts
+    });
 });
 
-// Route for rendering a sample blog post
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
 app.get('/samplepost', (req, res) => {
     res.render('samplepost');
 });
 
-// Route for rendering the blog post creation form
-const newPostController = require('./controllers/newPost');
-app.get('/posts/new', authMiddleware, newPostController)
-
-//the new controller functions
-app.get('/post/:id', getPostController)
-
-//the new controller functions
-app.post('/posts/store', authMiddleware, storePostController)
-
-const validateMiddleWare = (req, res, next) => {
-    if (req.files == null || req.body.title == null) {
-        return res.redirect('/posts/new')
-    }
-    next()
-};
-
-app.use('/posts/store', validateMiddleWare)
-
-//apply it to the route
-app.get('/auth/register', redirectIfAuthenticatedMiddleware, newUserController)
-
-app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController)
-
-app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController)
-
-app.use(expressSession({
-    secret: 'keyboard cat'
-})) //secret string is used by the express-session package to sign and encrypt the session ID cookie being shared with the browser
-
-global.loggedIn = null; //declare a global variable loggedIn that will be accessible from all our EJS files
-app.use("*", (req, res, next) => {
-    loggedIn = req.session.userId;
-    next()
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post', {
+        blogpost: blogpost
+    });
 });
+
+app.get('/posts/new', newPostController);
+
+app.get('/create', (req, res) => {
+    res.render('create');
+});
+
+app.post('/posts/store', async (req, res) => {
+    try {
+        let image = req.files.image;
+        await image.mv(path.resolve(__dirname, 'public/img', image.name));
+        await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name
+        });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/auth/register', newUserController);
+app.post('/users/register', storeUserController);
+
+app.get('/auth/login', loginController);
+app.post('/users/login', loginUserController);
 
 app.get('/auth/logout', logoutController);
 
-app.use((req, res) => res.render('notfound'));
+app.get('/posts/new', authMiddleware, newPostController);
 
-app.use(flash());
+app.post('/posts/store', authMiddleware, storePostController);
+
+app.get('/auth/register', redirectIfAuthenticatedMiddleware, newUserController);
+app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController);
+app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController);
+app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController);
+
+// The below routes seem to be duplicated, so you can remove them.
+// app.get('/posts/new', authMiddleware, newPostController);
+// app.post('/posts/store', authMiddleware, storePostController);
+// app.get('/auth/register', redirectIfAuthenticatedMiddleware, newUserController);
+// app.post('/users/register', redirectIfAuthenticatedMiddleware, storeUserController);
+// app.get('/auth/login', redirectIfAuthenticatedMiddleware, loginController);
+// app.post('/users/login', redirectIfAuthenticatedMiddleware, loginUserController);
+
+app.get('/auth/logout', logoutController);
+app.use((req, res) => res.render('notfound'));
